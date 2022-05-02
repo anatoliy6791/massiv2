90127b44d84bac3a194493ee3ff1afe4783fa486
user <anatoliy6791>
2022-05-02 12:39:01 +0300

    dz massiv 2.2

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -11,9 +11,9 @@ public class Main {
 
     public static void task1() {
         int sum = 0;
-        int[] random = {120_000, 140_000, 160_000, 150_000, 110_000, 100_000, 125_000, 145_000, 165_000, 155_000, 115_000, 105_000, 120_000, 140_000, 160_000, 150_000, 110_000, 100_000, 125_000, 145_000, 165_000, 155_000, 115_000, 105_000, 125_000, 145_000, 165_000, 155_000, 115_000, 105_000};
-        for (int i = 0; i < random.length - 1; i++) {
-            sum += random[i];
+        int array[] =generateRandomArray();
+        for (int i = 0; i < array.length - 1; i++) {
+            sum += array[i];
         }
         {
             System.out.println("Сумма трат за месяц составила " + sum + " рублей");
@@ -21,15 +21,16 @@ public class Main {
     }
 
     public static void task2() {
+
         int maxSum = 0;
         int minSum = 200_000;
-        int[] random = {120_000, 140_000, 160_000, 150_000, 110_000, 100_000, 125_000, 145_000, 165_000, 155_000, 115_000, 105_000, 120_000, 140_000, 160_000, 150_000, 110_000, 100_000, 125_000, 145_000, 165_000, 155_000, 115_000, 105_000, 125_000, 145_000, 165_000, 155_000, 115_000, 105_000};
-        for (int i = 0; i < random.length - 1; i++) {
-            if (random[i] > maxSum) {
-                maxSum = random[i];
+        int array[] =generateRandomArray();
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > maxSum) {
+                maxSum = array[i];
             }
-            if (random[i] < minSum) {
-                minSum = random[i];
+            if (array[i] < minSum) {
+                minSum = array[i];
             }
         }
         System.out.println("Минимальная сумма трат за день составила " + minSum + " рублей. Максимальная сумма трат за день составила " + maxSum + " рублей");
@@ -38,19 +39,30 @@ public class Main {
     public static void task3() {
         int sum = 0;
         double average = 0;
-        int[] random = {120_000, 140_000, 160_000, 150_000, 110_000, 100_000, 125_000, 145_000, 165_000, 155_000, 115_000, 105_000, 120_000, 140_000, 160_000, 150_000, 110_000, 100_000, 125_000, 145_000, 165_000, 155_000, 115_000, 105_000, 125_000, 145_000, 165_000, 155_000, 115_000, 105_000};
-        for (int i = 0; i < random.length - 1; i++) {
-            sum += random[i];
+        int array[] =generateRandomArray();
+        for (int i = 0; i < array.length - 1; i++) {
+            sum += array[i];
         }
         average = sum / 30;
         {
             System.out.println("Средняя сумма трат за месяц составила " + average + " рублей");
         }
+
     }
+
     public static void task4() {
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = 10; i>=0; i--){
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = 10; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
         }
+    }
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
 }
